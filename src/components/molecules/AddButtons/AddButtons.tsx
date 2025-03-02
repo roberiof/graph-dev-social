22972676e79b7baf85a33ca2246112b88122e153
Robério Filho <107323497+roberiof@users.noreply.github.com>
2025-03-02 17:48:10 -0300

    (fix): add buttons

--- src/components/molecules/AddButtons/AddButtons.tsx
+++ src/components/molecules/AddButtons/AddButtons.tsx
@@ -1,13 +1,14 @@
+import { developerColor, companyColor } from "@/common/constants/colors"
 import { AddButtonProps } from "./types"
 
 export const AddButton = ({ buttonType, ...props }: AddButtonProps) => {
   const colors = { 
-    developer: '#00ff00',
-    company: '#ff00ff'
+    developer: developerColor,
+    company: companyColor
   }
   return (
     <button className='flex items-center gap-2 bg-black/50 hover:bg-black/70 px-4 py-2 rounded-lg transition-all' {...props}>
-      <div className={`w-3 h-3 rounded-full bg-[${colors[buttonType]}]`}></div>
+      <div style={{backgroundColor: colors[buttonType]}} className="w-3 h-3 rounded-full"></div>
       <span>Add {buttonType}</span>
       <span className="text-xl">+</span>
     </button>
