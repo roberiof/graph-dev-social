f8a2852846292c9a8c339cb103986226603f6de8
Robério Filho <107323497+roberiof@users.noreply.github.com>
2025-03-02 17:17:34 -0300

    (feat): add validations and utils

---
+++ src/utils/utils.ts
@@ -0,0 +1,19 @@
+export const apiClient = {
+  get: async (endpoint: string) => {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}${endpoint}`);
+    return response.json();
+  },
+  post: async <T>(endpoint: string, data: T) => {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}${endpoint}`, {
+      method: 'POST',
+      body: JSON.stringify(data)
+    });
+    return response.json();
+  },
+  delete: async (endpoint: string) => {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}${endpoint}`, {
+      method: 'DELETE'
+    });
+    return response.json();
+  }
+};
